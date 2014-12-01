dfadd477c73e1a11b8334e6a7c77df883b5e67ca
Domenic Denicola <d@domenic.me>
2014-12-01 17:47:20 -0500

    Add Array.prototype.includes
    Requires adding a SameValueZero implementation.
    
    LOG=Y
    BUG=v8:3575
    R=rossberg@chromium.org, arv@chromium.org
    TEST=added to test262

--- src/harmony-array.js
+++ src/harmony-array.js
@@ -127,6 +127,41 @@ function ArrayFill(value /* [, start [, end ] ] */) {  // length == 1
   return array;
 }
 
+// Proposed for ES7
+// https://github.com/tc39/Array.prototype.includes
+// 6e3b78c927aeda20b9d40e81303f9d44596cd904
+function ArrayIncludes(searchElement, fromIndex) {
+  var array = ToObject(this);
+  var len = ToLength(array.length);
+
+  if (len === 0) {
+    return false;
+  }
+
+  var n = ToInteger(fromIndex);
+
+  var k;
+  if (n >= 0) {
+    k = n;
+  } else {
+    k = len + n;
+    if (k < 0) {
+      k = 0;
+    }
+  }
+
+  while (k < len) {
+    var elementK = array[k];
+    if (SameValueZero(searchElement, elementK)) {
+      return true;
+    }
+
+    ++k;
+  }
+
+  return false;
+}
+
 // ES6, draft 05-22-14, section 22.1.2.3
 function ArrayOf() {
   var length = %_ArgumentsLength();
@@ -150,8 +185,11 @@ function HarmonyArrayExtendArrayPrototype() {
     "of", ArrayOf
   ));
 
+  %FunctionSetLength(ArrayIncludes, 1);
+
   // Set up the non-enumerable functions on the Array prototype object.
   InstallFunctions($Array.prototype, DONT_ENUM, $Array(
+    "includes", ArrayIncludes,
     "find", ArrayFind,
     "findIndex", ArrayFindIndex,
     "fill", ArrayFill
