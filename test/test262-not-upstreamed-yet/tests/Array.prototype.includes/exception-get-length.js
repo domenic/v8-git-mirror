33b2aba1a041c22b3e5249e31d0707ad14ec9b96
Domenic Denicola <d@domenic.me>
2014-12-01 19:54:31 -0500

    Add some copyright headers; hacks

--- test/test262-not-upstreamed-yet/tests/Array.prototype.includes/exception-get-length.js
+++ test/test262-not-upstreamed-yet/tests/Array.prototype.includes/exception-get-length.js
@@ -1,3 +1,7 @@
+// Copyright 2014 the V8 project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // Copyright (C) 2014 Domenic Denicola. All rights reserved.
 // This code is governed by the BSD license found in the LICENSE file.
 
